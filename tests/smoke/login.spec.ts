87631b8525d4556c8784c76fef7e333f84485d94
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2024-02-09 18:51:07 +0100

    chore: test data for user mail

--- tests/smoke/login.spec.ts
+++ tests/smoke/login.spec.ts
@@ -1,12 +1,13 @@
 import { LoginPage } from '../../src/pages/login.page';
 import { WelcomePage } from '../../src/pages/welcome.page';
+import { testUser1 } from '../../src/test-data/user.data';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify login', () => {
   test('login with correct credentials @GAD-R02-01', async ({ page }) => {
     // Arrange
     const loginPage = new LoginPage(page);
-    const email = 'Dortha.Blick@Walker.name';
+    const email = testUser1.userEmail;
     const password = '5Damien43';
     await loginPage.goto();
 
