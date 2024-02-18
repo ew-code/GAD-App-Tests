52039e0b0935bb393015f27853fce6a6b20d2a62
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2024-02-18 14:23:48 +0100

    test: article test changes

--- tests/smoke/articles.spec.ts
+++ tests/smoke/articles.spec.ts
@@ -1,6 +1,7 @@
 import { ArticlesPage } from '../../src/pages/articles.page';
 import { LoginPage } from '../../src/pages/login.page';
 import { testUser1 } from '../../src/test-data/user.data';
+import { ArticleView } from '../../src/views/add-article.view';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify aricles', () => {
@@ -13,7 +14,13 @@ test.describe('Verify aricles', () => {
     const articlesPage = new ArticlesPage(page);
     await articlesPage.goto();
     await articlesPage.addArticleButtonLogged.click();
-    const title = await articlesPage.title();
-    expect(title).toContain('Welcome');
+    const addArticleView = new ArticleView(page);
+    await addArticleView.titleInput.fill('Test article about playwright');
+    await addArticleView.bodyText.fill(
+      'Playwright also offers a range of innovative features that make writing tests more flexible and powerful than ever before. For example, the ability to test user interactions with the keyboard and mouse, emulate touch device movements, and support multi-threaded testing are just some of the features that make Playwright such a unique tool.',
+    );
+    await addArticleView.saveButton.click();
+
+    await expect.soft(addArticleView.header).toBeVisible();
   });
 });
