490bdae3332ddbe0390d0961143198430180c6f6
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2024-02-26 19:34:12 +0100

    chore: locator name refactor

--- src/views/add-article.view.ts
+++ src/views/add-article.view.ts
@@ -2,7 +2,7 @@ import { AddArticleModel } from '../models/article.model';
 import { Page } from '@playwright/test';
 
 export class ArticleView {
-  header = this.page.getByRole('heading', { name: 'Add New Entry' });
+  addNewHeader = this.page.getByRole('heading', { name: 'Add New Entry' });
   titleInput = this.page.getByTestId('title-input');
   bodyText = this.page.getByTestId('body-text');
   saveButton = this.page.getByTestId('save');
