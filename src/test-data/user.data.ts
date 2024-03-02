7713d078730a2a783653d77f09a80da7fd455487
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2024-03-02 19:07:52 +0100

    chore: error handling for env variables

--- src/test-data/user.data.ts
+++ src/test-data/user.data.ts
@@ -1,6 +1,7 @@
+import { USER_EMAIL, USER_PASSWORD } from '../global.setup';
 import { LoginUserModel } from '../models/user.model';
 
 export const testUser1: LoginUserModel = {
-  userEmail: process.env.USER_EMAIL ?? '[NOT SET]',
-  userPassword: process.env.USER_PASSWORD ?? '[NOT SET]',
+  userEmail: USER_EMAIL,
+  userPassword: USER_PASSWORD,
 };
