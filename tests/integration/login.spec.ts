19ede63c62411db16d0ec43c8c99ddfa2034d03f
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2024-03-16 19:04:32 +0100

    chore: own fixtures in login tests

--- tests/integration/login.spec.ts
+++ tests/integration/login.spec.ts
@@ -1,15 +1,14 @@
+import { expect, test } from '@_src/fixtures/merge.fixture';
 import { LoginUserModel } from '@_src/models/user.model';
-import { LoginPage } from '@_src/pages/login.page';
 import { testUser1 } from '@_src/test-data/user.data';
-import { expect, test } from '@playwright/test';
 
 test.describe('Verify login', () => {
-  test('login with correct credentials @GAD-R02-01', async ({ page }) => {
+  test('login with correct credentials @GAD-R02-01', async ({ loginPage }) => {
     // Arrange
     const expectedWelcomeTitle = 'Welcome';
-    const loginPage = new LoginPage(page);
+    // const loginPage = new LoginPage(page);
     // const welcomePage = new WelcomePage(page);
-    await loginPage.goto();
+    // await loginPage.goto();
 
     // Act
     const welcomePage = await loginPage.login(testUser1);
@@ -19,16 +18,18 @@ test.describe('Verify login', () => {
     expect(title).toContain(expectedWelcomeTitle);
   });
 
-  test('reject login with incorrect password @GAD-R02-01', async ({ page }) => {
+  test('reject login with incorrect password @GAD-R02-01', async ({
+    loginPage,
+  }) => {
     // Arrange
     const expectedLoginTitle = 'Login';
-    const loginPage = new LoginPage(page);
+    // const loginPage = new LoginPage(page);
 
     const loginUserData: LoginUserModel = {
       userEmail: testUser1.userEmail,
       userPassword: 'incorrectPassword',
     };
-    await loginPage.goto();
+    // await loginPage.goto();
 
     // Act
     await loginPage.login(loginUserData);
