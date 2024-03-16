15dd91408d9c0b3f2f062d74333757387e536549
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2024-03-16 16:48:04 +0100

    chore: wip extraction fixtures

---
+++ src/fixtures/page-object.fixture.ts
@@ -0,0 +1,24 @@
+// import { Pages } from '@_src/models/pages.model';
+import { ArticlesPage } from '@_src/pages/articles.page';
+import { CommentsPage } from '@_src/pages/comments.page';
+import { test as baseTest } from '@playwright/test';
+
+interface Pages {
+  articlesPage: ArticlesPage;
+  commentsPage: CommentsPage;
+}
+
+export const pageObjectTest = baseTest.extend<Pages>({
+  articlesPage: async ({ page }, use) => {
+    const articlesPage = new ArticlesPage(page);
+    await articlesPage.goto();
+    // await use(new ArticlesPage(page));
+    await use(articlesPage);
+  },
+
+  commentsPage: async ({ page }, use) => {
+    const commentsPage = new CommentsPage(page);
+    await commentsPage.goto();
+    await use(commentsPage);
+  },
+});
--- tests/smoke/menu.spec.ts
+++ tests/smoke/menu.spec.ts
@@ -1,22 +1,19 @@
-import { Pages } from '@_src/models/pages.model';
-import { ArticlesPage } from '@_src/pages/articles.page';
-import { CommentsPage } from '@_src/pages/comments.page';
-import { test as baseTest, expect } from '@playwright/test';
+import { expect, test } from '@_src/fixtures/merge.fixture';
 
-const test = baseTest.extend<Pages>({
-  articlesPage: async ({ page }, use) => {
-    const articlesPage = new ArticlesPage(page);
-    await articlesPage.goto();
-    // await use(new ArticlesPage(page));
-    await use(articlesPage);
-  },
+// const test = baseTest.extend<Pages>({
+// articlesPage: async ({ page }, use) => {
+// const articlesPage = new ArticlesPage(page);
+// await articlesPage.goto();
+// await use(new ArticlesPage(page));
+// await use(articlesPage);
+// },
 
-  commentsPage: async ({ page }, use) => {
-    const commentsPage = new CommentsPage(page);
-    await commentsPage.goto();
-    await use(commentsPage);
-  },
-});
+// commentsPage: async ({ page }, use) => {
+// const commentsPage = new CommentsPage(page);
+// await commentsPage.goto();
+// await use(commentsPage);
+// },
+// });
 
 test.describe('Verify main menu buttons', () => {
   test('comments button navigates to comments page @GAD-R01-03', async ({
