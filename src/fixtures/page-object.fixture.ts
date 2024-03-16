10e468d169fe3dec9853234b71861e8c11fd2d8b
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2024-03-16 18:43:02 +0100

    chore: own fixtures in smoke tests

--- src/fixtures/page-object.fixture.ts
+++ src/fixtures/page-object.fixture.ts
@@ -1,11 +1,13 @@
 // import { Pages } from '@_src/models/pages.model';
 import { ArticlesPage } from '@_src/pages/articles.page';
 import { CommentsPage } from '@_src/pages/comments.page';
+import { HomePage } from '@_src/pages/home.page';
 import { test as baseTest } from '@playwright/test';
 
 interface Pages {
   articlesPage: ArticlesPage;
   commentsPage: CommentsPage;
+  homePage: HomePage;
 }
 
 export const pageObjectTest = baseTest.extend<Pages>({
@@ -20,5 +22,11 @@ export const pageObjectTest = baseTest.extend<Pages>({
     const commentsPage = new CommentsPage(page);
     await commentsPage.goto();
     await use(commentsPage);
+  },
+
+  homePage: async ({ page }, use) => {
+    const homePage = new HomePage(page);
+    await homePage.goto();
+    await use(homePage);
   },
 });
--- tests/smoke/webpage.spec.ts
+++ tests/smoke/webpage.spec.ts
@@ -1,42 +1,39 @@
-import { ArticlesPage } from '@_src/pages/articles.page';
-import { CommentsPage } from '@_src/pages/comments.page';
-import { HomePage } from '@_src/pages/home.page';
-import { expect, test } from '@playwright/test';
+import { expect, test } from '@_src/fixtures/merge.fixture';
 
 test.describe('Verify service main pages', () => {
-  test('home page title @GAD-R01-01', async ({ page }) => {
+  test('home page title @GAD-R01-01', async ({ homePage }) => {
     // Arrange
     const expectedHomePageTitle = 'GAD';
-    const homePage = new HomePage(page);
+    // const homePage = new HomePage(page);
 
     // Act
-    await homePage.goto();
+    // await homePage.goto();
 
     // Assert
     const title = await homePage.getTitle();
     expect(title).toContain(expectedHomePageTitle);
   });
 
-  test('articles page title @GAD-R01-02', async ({ page }) => {
+  test('articles page title @GAD-R01-02', async ({ articlesPage }) => {
     // Arrange
     const expectedArticlesTitle = 'Articles';
-    const articlesPage = new ArticlesPage(page);
+    // const articlesPage = new ArticlesPage(page);
 
     // Act
-    await articlesPage.goto();
+    // await articlesPage.goto();
 
     // Assert
     const title = await articlesPage.getTitle();
     expect(title).toContain(expectedArticlesTitle);
   });
 
-  test('comments page title @GAD-R01-02', async ({ page }) => {
+  test('comments page title @GAD-R01-02', async ({ commentsPage }) => {
     // Arrange
     const expectedCommentsTitle = 'Comments';
-    const commentsPage = new CommentsPage(page);
+    // const commentsPage = new CommentsPage(page);
 
     // Act
-    await commentsPage.goto();
+    // await commentsPage.goto();
 
     // Assert
     const title = await commentsPage.getTitle();
