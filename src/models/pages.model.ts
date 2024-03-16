50112a6b4561210357e20e23198c2377b57e537d
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2024-03-16 15:50:56 +0100

    chore: wip adding new page fixtures

---
+++ src/models/pages.model.ts
@@ -0,0 +1,7 @@
+import { ArticlesPage } from '@_src/pages/articles.page';
+import { CommentsPage } from '@_src/pages/comments.page';
+
+export interface Pages {
+  articlesPage: ArticlesPage;
+  commentsPage: CommentsPage;
+}
