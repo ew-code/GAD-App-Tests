7960153d0dc8fa5e3370a1e350952ec20515926a
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2024-03-17 18:59:31 +0100

    chore: article fixture lazy

--- src/fixtures/article.fixture.ts
+++ src/fixtures/article.fixture.ts
@@ -23,14 +23,14 @@ export const articleTest = pageObjectTest.extend<ArticleFixtures>({
     await use({ articlePage, articleData });
   },
 
-  // randomArticle: async ({ addArticleView }, use) => {
-  // const create = async (
-  // articleData?: AddArticleModel,
-  // ): Promise<ArticleCreationContext> => {
-  // const finalArticleData = articleData ?? prepareRandomArticle();
-  // const articlePage = await addArticleView.createArticle(finalArticleData);
-  // return { articlePage, articleData: finalArticleData };
-  // };
-  // await use(create);
-  // },
+  randomArticle: async ({ addArticleView }, use) => {
+    const create = async (
+      articleData?: AddArticleModel,
+    ): Promise<ArticleCreationContext> => {
+      const finalArticleData = articleData ?? prepareRandomArticle();
+      const articlePage = await addArticleView.createArticle(finalArticleData);
+      return { articlePage, articleData: finalArticleData };
+    };
+    await use(create);
+  },
 });
