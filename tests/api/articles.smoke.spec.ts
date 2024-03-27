62e3cd38d368508f3ca323ba7d74361629322e90
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2024-03-27 19:34:34 +0100

    test: api smoke articles extended test

--- tests/api/articles.smoke.spec.ts
+++ tests/api/articles.smoke.spec.ts
@@ -10,4 +10,19 @@ test.describe('Verify articles API endpoint @GAD-R08-01 @api', () => {
     // Assert
     expect(response.status()).toBe(expectedStatusCode);
   });
+
+  test('Get articles shouls reurn at least one article @predefined_data', async ({
+    request,
+  }) => {
+    // Arrange
+    const expectedMinArticleCount = 1;
+    const articlesUrl = '/api/articles';
+
+    // Act
+    const response = await request.get(articlesUrl);
+    const responseJson = await response.json();
+
+    // Assert
+    expect(responseJson.length).toBeGreaterThanOrEqual(expectedMinArticleCount);
+  });
 });
