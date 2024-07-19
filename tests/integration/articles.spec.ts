f36b0aa7037888aa56bfedb5f7afa526a8baccc4
ewCode <64744933+ew-code@users.noreply.github.com>
2024-07-19 11:15:34 +0200

    Update articles.spec.ts

--- tests/integration/articles.spec.ts
+++ tests/integration/articles.spec.ts
@@ -1,59 +1,8 @@
-// import { LoginPage } from '@_src/pages/login.page';
 import prepareRandomArticle from '@_src/factories/article.factory';
 import { expect, test } from '@_src/fixtures/merge.fixture';
 import { waitForResponse } from '@_src/utils/wait.util';
 
 test.describe('Verify articles', () => {
-  // let articlesPage: ArticlesPage;
-  // let addArticleView: ArticleView;
-  // let loginPage: LoginPage;
-
-  // test.beforeEach(async ({ addArticleView, articlesPage }) => {
-  // loginPage = new LoginPage(page);
-  // articlesPage = new ArticlesPage(page);
-  // addArticleView = new ArticleView(page);
-  //
-  // await loginPage.goto();
-  // await loginPage.login(testUser1);
-
-  // const sessionData = JSON.parse(fs.readFileSync('session.json', 'utf-8'));
-  // await page.context().addCookies(sessionData.cookies);
-  // await page.context().storageState(sessionData.localStorage);
-
-  // await articlesPage.goto();
-  // addArticleView = await articlesPage.clickAddArticleButtonLogged();
-  // await expect.soft(addArticleView.addNewHeader).toBeVisible();
-  // });
-
-  // test('reject creating article without title @GAD-R04-01', async () => {
-  // Arrange
-  // const expectedErrorText = 'Article was not created';
-
-  // Act
-  // await articlesPage.addArticleButtonLogged.click();
-  // await expect.soft(addArticleView.header).toBeVisible();
-  // const articleData = randomNewArticle();
-  // articleData.title = '';
-  // await addArticleView.saveButton.click();
-
-  // Assert
-  // await expect(addArticleView.alertPopUp).toHaveText(expectedErrorText);
-  // });
-
-  // test('reject creating article without body @GAD-R04-01', async () => {
-  // Arrange
-  // const expectedErrorText = 'Article was not created';
-
-  // Act
-  // await expect.soft(addArticleView.header).toBeVisible();
-  // const articleData = randomNewArticle();
-  // articleData.body = '';
-  // await addArticleView.saveButton.click();
-
-  // Assert
-  // await expect(addArticleView.alertPopUp).toHaveText(expectedErrorText);
-  // });
-
   test('reject creating article without body @GAD-R04-01 @logged', async ({
     addArticleView,
     page,
@@ -121,7 +70,6 @@ test.describe('Verify articles', () => {
       page,
     }) => {
       // Arrange
-      // const articlePage = new ArticlePage(page);
       const expectedErrorMessage = 'Article was created';
       const expectedResponseCode = 201;
       const articleData = prepareRandomArticle(128);
